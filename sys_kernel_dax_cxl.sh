0a0c6ecbbae96f2b89f7e4c5b88a87b71339568c
Roger C. Pao <roger.pao@smartm.com>
2024-06-12 22:36:42 -0700

    sys_kernel_dax_cxl.sh
    This script is used to fill in the test matrix spreadsheet at
    https://smartm-my.sharepoint.com/:x:/r/personal/roger_pao_smartm_com/Documents/CXL/sys_kernel_dax_cxl_mchp.xlsx?d=w4d85d9491ad44829a2f522abdc8cb4af&csf=1&web=1&e=GvZyNK

---
+++ sys_kernel_dax_cxl.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+# script sys_kernel_dax_cxl_$(date +%Y%m%d-%H%M%S)_$(hostname)_$(uname -r).txt
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+LOGFILE=sys_kernel_dax_cxl_${TIMESTAMP}_$(hostname)_$(uname -r).txt
+
+
+function docmd() {
+  CMD=$1
+  echo "$ ${CMD}" | tee -a ${LOGFILE}
+  ${CMD} | tee -a ${LOGFILE}
+}
+
+
+docmd "hostname"
+docmd "whoami"
+docmd "ip a"
+docmd "sudo dmidecode"
+docmd "grep PRETTY_NAME /etc/os-release"
+docmd "uname -r"
+
+docmd "lspci"
+# pipe | does not work in docmd()
+# docmd "lspci\|grep -i cxl"
+echo "$ lspci|grep -i cxl" | tee -a ${LOGFILE}
+lspci|grep -i cxl | tee -a ${LOGFILE}
+
+docmd "lsmod"
+# docmd "lsmod|grep dax"
+echo "$ lsmod|grep dax" | tee -a ${LOGFILE}
+lsmod|grep dax | tee -a ${LOGFILE}
+
+docmd "ls -l /dev/dax0.0"
+docmd "daxctl list"
+
+# docmd "lsmod|grep cxl"
+echo "$ lsmod|grep cxl" | tee -a ${LOGFILE}
+lsmod|grep cxl | tee -a ${LOGFILE}
+
+docmd "ls -l /dev/cxl/mem0"
+docmd "sudo cxl list -vvvv"
+
+docmd "sudo lspci -vvv"
+# docmd "sudo lspci -vvv|grep CXLCtl"
+# $ sudo lspci -vvv|grep CXLCtl
+# lspci: invalid option -- '|'
+# Usage: lspci [<switches>]
+# man sudo example: sudo sh -c "cd /home ; du -s * | sort -rn > USAGE"
+echo "$ sudo lspci -vvv|grep CXLCtl" | tee -a ${LOGFILE}
+sudo lspci -vvv|grep CXLCtl | tee -a ${LOGFILE}
+
+# exit # end script
